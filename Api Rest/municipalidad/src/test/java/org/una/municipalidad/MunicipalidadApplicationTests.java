67d1cd3af1a95ea0d5e63b4dfbe43ba507c087a0
joshua292000 <joshua292000@gmail.com>
2021-09-17 16:43:04 -0600

    Create MunicipalidadApplicationTests.java

---
+++ Api Rest/municipalidad/src/test/java/org/una/municipalidad/MunicipalidadApplicationTests.java
@@ -0,0 +1,13 @@
+package org.una.municipalidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunicipalidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
