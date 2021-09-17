4196cd048a30b53dba249cf0490f4c113a145817
joshua292000 <joshua292000@gmail.com>
2021-09-17 16:43:37 -0600

    Create EscritorioApplicationTests.java

---
+++ App de Escritorio/escritorio/src/test/java/org/una/escritorio/EscritorioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.una.escritorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscritorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
