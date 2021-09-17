82e876bd7b1a911178be6ab06c894bf8a9592b2f
joshua292000 <joshua292000@gmail.com>
2021-09-17 16:43:31 -0600

    Create EscritorioApplication.java

---
+++ App de Escritorio/escritorio/src/main/java/org/una/escritorio/EscritorioApplication.java
@@ -0,0 +1,13 @@
+package org.una.escritorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscritorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscritorioApplication.class, args);
+	}
+
+}
