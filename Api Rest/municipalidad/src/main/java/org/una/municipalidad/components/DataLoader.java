ad40d1d3ff4d4e26aa5dbb11eb567f5d3d6b641d
joshua292000 <joshua292000@gmail.com>
2021-10-09 21:54:08 -0600

    correciones

--- Api Rest/municipalidad/src/main/java/org/una/municipalidad/components/DataLoader.java
+++ Api Rest/municipalidad/src/main/java/org/una/municipalidad/components/DataLoader.java
@@ -37,39 +37,51 @@ public class DataLoader implements ApplicationRunner {
             Optional<DepartamentoDTO> cajasDepartamento = departamentoService.create(DepartamentoDTO.builder().nombre("Cajas").build());
             Optional<DepartamentoDTO> informaticaDepartamento = departamentoService.create(DepartamentoDTO.builder().nombre("Informatica").build());
 */
-            Optional<RolesDTO> colaboradorRol = rolService.create(RolesDTO.builder().nombreRol("Colaborador").build());
-            Optional<RolesDTO> auditorRol = rolService.create(RolesDTO.builder().nombreRol("Auditor").build());
-            Optional<RolesDTO> contadorRol = rolService.create(RolesDTO.builder().nombreRol("Contador").build());
-            Optional<RolesDTO> usuarioRol = rolService.create(RolesDTO.builder().nombreRol("Usuario").build());
+            Optional<RolesDTO> gestorRol = rolService.create(RolesDTO.builder().nombreRol("Gestor").build());
+            Optional<RolesDTO> gerenteRol = rolService.create(RolesDTO.builder().nombreRol("Gerente").build());
             Optional<RolesDTO> administradorRol = rolService.create(RolesDTO.builder().nombreRol("Administrador").build());
+            Optional<RolesDTO> usuarioRol = rolService.create(RolesDTO.builder().nombreRol("Usuario").build());
+            Optional<RolesDTO> auditorRol = rolService.create(RolesDTO.builder().nombreRol("Auditor").build());
 
             UsuariosDTO cajeroUsuario = UsuariosDTO.builder()
-
                     .cedula("0123456789")
-                    .nombreUsuario("Josselyne Mora")
-                    .claveEncriptado("SoylaJoss")
+                    .nombreUsuario("Panchito")
+                    .claveEncriptado("agapito123")
                     .estado(true)
-                    //.departamento(cajasDepartamento.orElseThrow())
                     .roles(usuarioRol.orElseThrow()).build();
             usuarioService.create(cajeroUsuario);
 
-            UsuariosDTO contadorUsuario = UsuariosDTO.builder()
+            UsuariosDTO auditorUsuario = UsuariosDTO.builder()
+                    .cedula("0948242")
+                    .nombreUsuario("Panchita")
+                    .claveEncriptado("agapita123")
+                    .estado(true)
+                    .roles(auditorRol.orElseThrow()).build();
+            usuarioService.create(auditorUsuario);
+
+            UsuariosDTO gestorUsuario = UsuariosDTO.builder()
                     .cedula("9876543210")
-                    .nombreUsuario("Kevin Mora")
+                    .nombreUsuario("Kevin")
                     .claveEncriptado("SoyelKevin")
                     .estado(true)
-                    //.departamento(contabilidadDepartamento.orElseThrow())
-                    .roles(contadorRol.orElseThrow()).build();
-            usuarioService.create(contadorUsuario);
+                    .roles(gestorRol.orElseThrow()).build();
+            usuarioService.create(gestorUsuario);
 
             UsuariosDTO administradorUsuario = UsuariosDTO.builder()
                     .cedula(cedula)
-                    .nombreUsuario("Joshua Granados")
+                    .nombreUsuario("Joshua")
                     .claveEncriptado(claveEncriptado)
                     .estado(true)
-                   // .departamento(informaticaDepartamento.orElseThrow())
                     .roles(administradorRol.orElseThrow()).build();
             usuarioService.create(administradorUsuario);
+
+            UsuariosDTO gerenteUsuario = UsuariosDTO.builder()
+                    .cedula("11833091")
+                    .nombreUsuario("Josselyn")
+                    .claveEncriptado("Nadiepasadeestaesquina")
+                    .estado(true)
+                    .roles(gerenteRol.orElseThrow()).build();
+            usuarioService.create(gerenteUsuario);
 
             System.out.println("Se agrega el usuario inicial a la aplicación");
         }else {
