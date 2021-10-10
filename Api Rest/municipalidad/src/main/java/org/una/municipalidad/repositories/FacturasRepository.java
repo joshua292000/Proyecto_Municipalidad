ea2bdd21cd478ca192d6cb57cfb02c54a4354d35
Josselyn Mora S <jossems.1012@gmail.com>
2021-10-09 23:24:11 -0600

    Tipo de cobros y Facturas repository

---
+++ Api Rest/municipalidad/src/main/java/org/una/municipalidad/repositories/FacturasRepository.java
@@ -0,0 +1,13 @@
+package org.una.municipalidad.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.una.municipalidad.entities.Facturas;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Repository
+public interface FacturasRepository extends JpaRepository<Facturas, Long> {
+    public Optional<Facturas> findByFecha(Date fechapago);
+}
