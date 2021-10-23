dc9ce5ff3c6df8995598ed81260b3a873f3a170f
joshua292000 <joshua292000@gmail.com>
2021-10-22 23:03:48 -0600

    Consultas

---
+++ Robot de Telegram/robot/src/Parametros.ts
@@ -0,0 +1,5 @@
+export interface parametros{
+    id: number;
+    llave_parametro: string;
+    valor_parametro: string;
+}
--- Robot de Telegram/robot/src/consultas_service.ts
+++ Robot de Telegram/robot/src/consultas_service.ts
@@ -1,5 +1,31 @@
 import axios from 'axios';
+import {Telegraf} from 'telegraf'
+import {usuario} from './usuario';
+import { parametros } from './Parametros';
 
-export class ConsultasService{
-
+export class consultas_service{
+    Horario(token: string, parameters: string, bot: Telegraf){
+        axios.get('http://localhost:8089/parametros/' + parameters[1], 
+        {headers: {
+            Authorization: 'bearer ' + token,
+        }}).then(response => {
+            var parametro = response.data as parametros;
+            bot.telegram.sendMessage(parametro.Horario);
+        })
+        .catch(err => {
+            console.log(err, err.response);
+        });
+    }
+    Formula(token: string, parameters: string, bot: Telegraf){
+        axios.get('http://localhost:8089/parametros/'+parameters[1], {headers: {    
+          Authorization: 'bearer ' + token,
+        }}).then(response => {
+          var param = response.data as parametro;
+          bot.telegram.sendMessage(param.Formula);
+        })
+        .catch(err => {
+          console.log(err, err.response);
+        });
+  
+    }
 }
