f6cece2e17f0e721062a6845839118a0994ee70d
Josselyn Mora S <jossems.1012@gmail.com>
2021-10-30 16:02:43 -0600

    Base

---
+++ App de Escritorio/escritorio/src/main/java/org/una/municipalidad/app_escritorio/Controller/LogginController.java
@@ -0,0 +1,19 @@
+package org.una.municipalidad.app_escritorio.Controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LogginController extends Controller implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+}
---
+++ App de Escritorio/escritorio/src/main/java/org/una/municipalidad/app_escritorio/HelloController.java
@@ -0,0 +1,14 @@
+package org.una.municipalidad.app_escritorio;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
